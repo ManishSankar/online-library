1bfd1eef9cd202503f1d975092c55eecab1af24d
ManishSankar <manish.sankar@capgemini.com>
2020-05-29 16:08:14 +0530

    date issue resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,32 +41,32 @@ export class AppComponent implements OnInit{
         field: 'authorName',
         maxWidth: 100,
       },
-    //   {
-    //     field: 'date',
-    //     filter: 'agDateColumnFilter',
-    //     filterParams: {
-    //       comparator: function(filterLocalDateAtMidnight, cellValue) {
-    //         var dateAsString = cellValue;
-    //         if (dateAsString == null) return -1;
-    //         var dateParts = dateAsString.split('/');
-    //         var cellDate = new Date(
-    //           Number(dateParts[2]),
-    //           Number(dateParts[1]) - 1,
-    //           Number(dateParts[0])
-    //         );
-    //         if (filterLocalDateAtMidnight.getTime() == cellDate.getTime()) {
-    //           return 0;
-    //         }
-    //         if (cellDate < filterLocalDateAtMidnight) {
-    //           return -1;
-    //         }
-    //         if (cellDate > filterLocalDateAtMidnight) {
-    //           return 1;
-    //         }
-    //       },
-    //       browserDatePicker: true,
-    //     },
-    //   },
+      {
+        field: 'publishDate',
+        filter: 'agDateColumnFilter',
+        filterParams: {
+          comparator: function(filterLocalDateAtMidnight, cellValue) {
+            var dateAsString = cellValue;
+            if (dateAsString == null) return -1;
+            var dateParts = dateAsString.split('/');
+            var cellDate = new Date(
+              Number(dateParts[2]),
+              Number(dateParts[1]) - 1,
+              Number(dateParts[0])
+            );
+            if (filterLocalDateAtMidnight.getTime() == cellDate.getTime()) {
+              return 0;
+            }
+            if (cellDate < filterLocalDateAtMidnight) {
+              return -1;
+            }
+            if (cellDate > filterLocalDateAtMidnight) {
+              return 1;
+            }
+          },
+          browserDatePicker: true,
+        },
+      },
       { field: 'publisherName' },
       {
         field: 'totalPage',
